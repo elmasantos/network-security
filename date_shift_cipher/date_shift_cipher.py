16c8b66f39723b9e623f0e63fff256937ac36bb2
Elma Santos <elmasantos94@hotmail.com>
2018-09-02 16:05:33 -0300

    Add date shift cipher and decipher script

---
+++ date_shift_cipher/date_shift_cipher.py
@@ -0,0 +1,28 @@
+import sys
+
+input_string = sys.argv[1]
+input_file = open(sys.argv[2], 'r')
+
+date = input_string.replace("/", "")
+
+def clear_text(input_file):
+    clear_text = input_file.read().replace(" ", "")
+    return clear_text.lower()
+
+def expanded_date(date, length):
+    return (date * (int(length/len(date))+1))[:length]
+
+def crypt(expanded_date):
+    crypt_text = ""
+    for i, letter in enumerate(clear_text):
+        if letter.isalpha():
+            unicode_int = ord(letter) + int(expanded_date[i])
+            crypt_text += chr(unicode_int)
+    return crypt_text
+
+clear_text = clear_text(input_file)
+expanded_date = expanded_date(date, len(clear_text))
+
+f = open('encrypted_text.sec', 'w')
+f.write(crypt(expanded_date))
+f.close()
---
+++ date_shift_cipher/date_shift_decipher.py
@@ -0,0 +1,27 @@
+import sys
+
+input_string = sys.argv[1]
+input_file = open(sys.argv[2], 'r')
+
+date = input_string.replace("/", "")
+
+def encrypted_text(input_file):
+    return input_file.read()
+
+def expanded_date(date, length):
+    return (date * (int(length/len(date))+1))[:length]
+
+def decrypt(expanded_date):
+    crypt_text = ""
+    for i, letter in enumerate(encrypted_text):
+        if letter.isalpha():
+            unicode_int = ord(letter) - int(expanded_date[i])
+            crypt_text += chr(unicode_int)
+        else:
+            crypt_text += " "
+    return crypt_text
+
+encrypted_text = encrypted_text(input_file)
+expanded_date = expanded_date(date, len(encrypted_text))
+
+print(decrypt(expanded_date))
